8bcf10374f48c969a3f9c1131aef8fc429abf651
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-23 18:04:13 -0300

    "Criado getters and setters"

--- src/main/java/com/kafka/springbootkafkaproducerexample/model/User.java
+++ src/main/java/com/kafka/springbootkafkaproducerexample/model/User.java
@@ -10,4 +10,28 @@ public class User {
         this.dept = dept;
         this.salary = salary;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
 }
