e4a0fada00e16fcb46b2764dd5fe0cc0ff898866
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-23 18:50:10 -0300

    "Tanto o StringSerializer como o JsonSerializer tem de ser do org.apache.kafka.common.serialization e rg.springframework.kafka... respectivamente"

--- src/main/java/com/kafka/springbootkafkaproducerexample/config/KafkaConfiguration.java
+++ src/main/java/com/kafka/springbootkafkaproducerexample/config/KafkaConfiguration.java
@@ -1,15 +1,14 @@
 package com.kafka.springbootkafkaproducerexample.config;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import com.kafka.springbootkafkaproducerexample.model.User;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
