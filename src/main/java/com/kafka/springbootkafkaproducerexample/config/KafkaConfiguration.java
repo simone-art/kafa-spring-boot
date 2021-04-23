5e1bba9b764d4a70e714ac3afbd9c494c7b2f064
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-23 18:27:29 -0300

    "Criada classe pra configurar o Bootstrap, Key y Value Serializer do Kafka"

---
+++ src/main/java/com/kafka/springbootkafkaproducerexample/config/KafkaConfiguration.java
@@ -0,0 +1,31 @@
+package com.kafka.springbootkafkaproducerexample.config;
+
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Bean
+    public ProducerFactory producerFactory(){
+
+        Map<String, Object> config = new HashMap<>();
+        //9092 é a mesma porta que está sendo usada pelo consumer é so ir lá e verificar
+        //StringSerializer.class vem do própio Spring
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+
+        return new DefaultKafkaProducerFactory(config);
+    }
+}
